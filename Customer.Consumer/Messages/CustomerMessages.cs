1b063f3d89e1637e71ca16486647172b5b4478ac
fmotta <fmotta@ALUGA-061560>
2023-11-16 17:31:24 -0300

    Adicionar arquivos de projeto.

---
+++ Customer.Consumer/Messages/CustomerMessages.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+
+namespace Customers.Consumer.Messages;
+
+public class CustomerCreated : ISqsMessage
+{
+    public required Guid Id { get; init; }
+    public required string GitHubUsername { get; init; }
+    public required string FullName { get; init; }
+    public required string Email { get; init; }
+    public required DateTime DateOfBirth { get; set; }
+}
+
+public class CustomerUpdated : ISqsMessage
+{
+    public required Guid Id { get; init; }
+    public required string GitHubUsername { get; init; }
+    public required string FullName { get; init; }
+    public required string Email { get; init; }
+    public required DateTime DateOfBirth { get; set; }
+}
+
+public class CustomerDeleted : ISqsMessage
+{
+    public required Guid Id { get; init; }
+}
+
